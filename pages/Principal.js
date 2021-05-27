2c63bd01b5fe368c2bb9d96668ed83415800ec55
Nicolas <Espada_2255@hotmail.com>
2021-05-27 18:59:16 -0400

    Introduciendo el file upload, necesito consultar un poco al profe antes de avanzar

--- pages/Principal.js
+++ pages/Principal.js
@@ -1,3 +1,79 @@
+import Head from 'next/head'
+
 export default function Principal() {
-    return <h1>Empezemos...</h1>
+    
+  return (
+    <div className="container">
+      <Head>
+        <title>Sistema FuSA Valdivia</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Fusa Valdivia
+        </h1>
+
+
+        <body>
+            <form action="/subida" method="POST" enctype="multipart/form-data">
+                <input type="file" name="Audio" />
+                <button type="submit">Submit</button>
+            </form>
+        </body>
+
+      </main>
+      <style jsx>{`
+        .container {
+          min-height: 10vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 5.00;
+          font-size: 2rem;
+        }
+        
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+    )
   }
