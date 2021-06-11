4b36016a782586ce49390dd65f30810b630402bc
PabloSzx <pablosaez1995@gmail.com>
2021-06-11 09:20:46 -0400

    arreglos hechos en taller

--- pages/Principal.js
+++ pages/Principal.js
@@ -1,35 +1,29 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import dynamic from "next/dynamic";
-import * as React from "react"
+import * as React from "react";
+
+const MapWithNoSSR = dynamic(() => import("../components/DatosM").then((v) => v.Map), {
+  ssr: false,
+});
 
 export default function Principal() {
-  const MapWithNoSSR = dynamic(() => import("../components/DatosM"), {
-    ssr: false
-  });
   return (
     <div className="container">
       <Head>
         <title>Sistema FuSA Valdivia</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="title">
-          FuSA Valdivia
-        </h1>
+      <h1 className="title">FuSA Valdivia</h1>
       <main>
-
         <div className="grid">
-
           <div className="Mapa" id="map">
             <MapWithNoSSR />
           </div>
-
-
         </div>
-
       </main>
-      
+
       <footer>
         <a href="/Subidas" className="card">
           <h3>Subir Audio &rarr;</h3>
@@ -53,16 +47,14 @@ export default function Principal() {
           align-items: center;
         }
         .title {
-          line-height: 5.00;
+          line-height: 5;
           font-size: 2rem;
           flex: 1;
           display: flex;
           justify-content: center;
           align-items: center;
         }
-        
-        
-        
+
         footer {
           width: 100%;
           height: 100px;
@@ -83,7 +75,7 @@ export default function Principal() {
           border: 5px solid #eaeaea;
           border-radius: 20px;
           transition: color 0.15s ease, border-color 0.15s ease;
-          
+
           max-width: 300px;
         }
         .card:hover,
@@ -99,9 +91,8 @@ export default function Principal() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
         }
 
         * {
@@ -109,5 +100,5 @@ export default function Principal() {
         }
       `}</style>
     </div>
-    )
-  }
+  );
+}
